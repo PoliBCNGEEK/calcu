9dcb884a054ebe10274090c7fb3e54221fde0462
poliernus <polmeca@gmail.com>
2022-07-21 11:13:13 +0200

    adasdad

---
+++ scripts/nuevo.js
@@ -0,0 +1,4 @@
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
